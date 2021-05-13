db0587dbb7fcaac6dc7b109bbd532622397fe5f1
FernandoENF <fernandoengh@gmail.com>
2021-05-13 19:07:38 -0300

    deploy

--- server/index.js
+++ server/index.js
@@ -33,10 +33,12 @@ app.use('/api',rotas)
 
 app.use(express.static(__dirname+'/cliente'))
 
-app.get("/", function(req,res){
-    res.send("hello world")
-})
+app.use(express.static(path.join(__dirname, 'build')));
 
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(8081,function(){
     console.log("Servidor Rodando na url http://localhost:8081")
